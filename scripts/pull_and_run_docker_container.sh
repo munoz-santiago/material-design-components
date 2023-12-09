608b7c950758809328711df652209f10ba283334
Santiago  Muñoz <smunoz@Santiagos-MacBook-Pro.local>
2023-12-09 11:31:29 -0500

    Build docker image, publish to ghcr and deploy it as a docker container in a digital ocean droplet

---
+++ scripts/pull_and_run_docker_container.sh
@@ -0,0 +1,15 @@
+
+echo "$GHCR_PRIVATE_TOKEN" | docker login -u "$GHCR_USERNAME" --password-stdin ghcr.io
+
+# Pull the Docker image from GitHub Container Registry
+docker pull $DOCKER_IMAGE
+
+# Stop and remove the existing container (if any)
+docker stop material-design-components-storybook || true
+docker rm -f material-design-components-storybook || true
+
+# Run the Docker container on the droplet
+docker run -d --name material-design-components-storybook -p 9002:80 \
+    --network traefik_proxy \
+    --label "traefik.http.routers.material-design-components-storybook.rule=PathPrefix(\`/react-components\`)" \
+    ghcr.io/munoz-santiago/material-design-components/storybook
