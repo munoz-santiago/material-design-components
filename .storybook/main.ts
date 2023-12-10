4682535a7dde0ce8e1ff908bb14cbe485bef883d
Santiago  Muñoz <smunoz@Santiagos-MacBook-Pro.local>
2023-12-10 09:15:26 -0500

    .

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -22,7 +22,7 @@ const config: StorybookConfig = {
   },
   staticDirs: ["../public"],
   webpackFinal: async (config, { configType }) => {
-    // config!.output!.publicPath = '/react-components';
+    config!.output!.publicPath = '/react-components';
     return config;
   },
 };
